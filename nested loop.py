3a849696135bde443fc0753785254b44244b3893
Dharshini Mangalathar <dharshiniramesh0809@gmail.com>
2026-08-12 21:18:40 +0530

    Add files via upload

---
+++ nested loop.py
@@ -0,0 +1,13 @@
+for i in range(1,4):
+    print("week:",i)
+    for j in range(1,4):
+     print("day:",j)
+    
+
+for i in range(1,5):
+    print("*")
+    for j in range(i):
+        print(j,end="")
+    
+    
+    
